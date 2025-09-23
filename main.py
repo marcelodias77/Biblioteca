92bae5c4dedef1c248eaf4c761f26157892a6159
Marcelo Dias <mdiad2733@gmail.com>
2025-09-23 08:58:15 -0300

    add criar tabela

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import sqlite3
 conexao = sqlite3.connect("biblioteca.db")
 
 cursor = conexao.cursor()
+
+def criar_tabela():
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS alunos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTEGER,
+    disponivel TEXT
+        )
+    """)
