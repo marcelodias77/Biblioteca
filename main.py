4a9d1b625ce9ad9006c6fbb95c030a877949e83b
Marcelo Dias <mdias2733@gamil.com>
2025-09-29 09:58:00 -0300

    etapa 2 - cadastro de livro

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sqlite3
 
 
-conexao = sqlite3.connect("biblioteca.db")
-cursor = conexao.cursor()
+
 
 #"Etapa 1 - Criação do banco e tabela"
 def criar_tabela():
+    conexao = sqlite3.connect("biblioteca.db")
+    cursor = conexao.cursor()
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS alunos (
+    CREATE TABLE IF NOT EXISTS livros (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo TEXT NOT NULL,
     autor TEXT NOT NULL,
@@ -15,5 +16,21 @@ def criar_tabela():
     disponivel TEXT
         )
     """)
-criar_tabela()
-
+    conexao.commit()
+#Etapa 2 - Cadastro de livros
+def cadastra_livro():
+    conexao = sqlite3.connect("biblioteca.db")
+    cursor = conexao.cursor()
+    
+    titu = input("Digite o titulo do livro: ")
+    aut = input("Digite o nome do autor: ")
+    ano = int(input("Digite o ano do livro: "))
+    dispo = input("Digite se o livro está disponivel: ")
+    cursor.execute("""
+    INSERT INTO livros (titulo, autor, ano, disponivel)
+    VALUES (?, ?, ?, ?)
+    """, (titu, aut, ano, dispo)
+    )
+    conexao.commit()
+    conexao.close()
+cadastra_livro()
